1c63b82efce3a78b4769f5512ad5bc39c1046405
Basheqalglil <m.sabaa.kiwan@gmail.com>
2017-06-11 16:55:07 +0200

    p

--- Nn/vgg.cc
+++ Nn/vgg.cc
@@ -13,8 +13,26 @@ void vgg::createTemporalFramesFromVideo(const std::string& filename){
    Video video ;
    readVideo(filename,video);
 
+   for (int i = 0 ; i < video.size()-11,i++){
+       cv::Mat c10Image(video[0].rows*20,video[0].cols,CV_8U);
+       for (int l = 0 ; l < 10 ; l ++){
+           cv::Mat tmpFlow;
+           cv::Mat tmpXY[2];
+           cv::calcOpticalFlowFarneback(video.at(i+l), video.at(i+l+1), tmpFlow, 0.702, 5, 10, 2, 7, 1.5, cv::OPTFLOW_FARNEBACK_GAUSSIAN );
+           cv::split(tmpFlow, tmpXY);
 
+           for (int ii = 0 ; ii < video.at(0).rows,ii++){
+               for (int jj = 0 ; jj < viedo.at(0).cols, jj++){
+                   c10Image.at<uchar>((ii*20)+l,jj) = tmpXY[0].at<uchar>(ii,jj);
+                   c10Image.at<uchar>((ii*20)+l+1,jj) = tmpXY[1].at<uchar>(ii,jj);
+               }
+           }
+       }
+       chat buff[3];
+       sprintf(buff,"%3d",i);
 
+       imwrite(filename +buff,c10Image);
+   }
 }
 void vgg::readVideo(const std::string& filename, Video& result) {
     // open video file
